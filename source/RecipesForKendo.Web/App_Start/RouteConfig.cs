a510bd363f78f0a72d0ff109fde1ef5fe9f23cc0
hanssens <github@hanssens.com>
2015-02-05 17:17:22 +0100

    Enable lowercase-urls by default

--- source/RecipesForKendo.Web/App_Start/RouteConfig.cs
+++ source/RecipesForKendo.Web/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace RecipesForKendo.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.LowercaseUrls = true;
 
             routes.MapRoute(
                 name: "Default",
