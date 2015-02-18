05ae22e1c8638a9f0363fe123e21f93bf7ae5f89
hanssens <github@hanssens.com>
2015-02-18 14:57:26 +0100

    Initial setup for a working, real life sample of multi-list drag and drop - Works, now requires some styling and cleanup

---
+++ source/RecipesForKendo.Web/Controllers/DragAndDropController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecipesForKendo.Web.Controllers
+{
+    public class DragAndDropController : Controller
+    {
+        // GET: DragAndDrop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
